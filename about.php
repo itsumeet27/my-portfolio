04a8005dc4ce1262c5880936db26ce20064dae57
Sumeet Sharma <sksksharma0@gmail.com>
2020-09-10 23:43:20 +0530

    About section

--- about.php
+++ about.php
@@ -1,9 +1,24 @@
 <?php include('includes/header.php'); ?>
-    <div class="page-title p-4">
-        <h1 class="p-2">ABOUT <hr></h1>
-        
-    </div>
-    <div class="container">
-
+    <div class="container-fluid">
+        <div class="page-title p-4">
+            <h1 class="p-2">ABOUT <hr></h1>        
+        </div>
+        <div class="about mt-2 px-4">
+            <div class="row">
+                <div class="col-md-6 p-1">
+                <img src="img/itsumeet.jpg" class="img-fluid img-responsive p-3"> 
+                </div>
+                <div class="col-md-6 p-3">
+                    <i class="fas fa-quote-left fa-2x"></i>
+                    <p class="px-3 py-3">
+                        Experienced Web Developer with a demonstrated history of working in the higher education industry. Skilled in Web framework such as Django, and PHP, WordPress, Java, HTML, CSS, Javascript, Bootstrap, Responsive Web Design, and Leadership. Strong engineering professional with an Engineer’s Degree focused in Information Technology from St. Francis Institute Of Technology.
+                    </p>
+                    <p class="px-3">
+                        I am a confident individual with an intent of delivering best of my work with the help of knowledge and ability in an efficient manner. Also, I am an artist with collection of drawings that might interest you!
+                    </p>
+                    <a href="http://drawingsbysumeet.itsumeet.com/" class="btn btn-portfolio btn-lg ml-3">Browse Now</a>
+                </div>
+            </div>
+        </div>
     </div>
 <?php include('includes/footer.php'); ?>
