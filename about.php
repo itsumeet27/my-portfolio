7e731ded3d50368dd9a5a36dd657c6b20b55e7e3
Sumeet Sharma <sksksharma0@gmail.com>
2020-09-11 01:09:03 +0530

    About section

--- about.php
+++ about.php
@@ -3,6 +3,8 @@
         <div class="page-title p-4">
             <h1 class="p-2">ABOUT <hr></h1>        
         </div>
+
+        <!-- About -->
         <div class="about mt-2 px-4">
             <div class="row">
                 <div class="col-md-6 p-1">
@@ -17,6 +19,91 @@
                         I am a confident individual with an intent of delivering best of my work with the help of knowledge and ability in an efficient manner. Also, I am an artist with collection of drawings that might interest you!
                     </p>
                     <a href="http://drawingsbysumeet.itsumeet.com/" class="btn btn-portfolio btn-lg ml-3">Browse Now</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Skills -->
+        <div class="page-title px-4 py-3 mt-5">
+            <h3 class="p-2">SKILLS <hr></h3>        
+        </div>
+        <div class="skills px-4 py-2">
+            <div class="row">
+                <div class="col-md-6 px-4">
+                    <div class="technologies mb-4">
+                        <p class="">HTML5</p>
+                        <div class="progress" style="height: 20px;">
+                            <div class="progress-bar" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                            90%
+                            </div>
+                        </div>
+                    </div>                    
+                    <div class="technologies mb-4">
+                        <p class="">CSS3</p>
+                        <div class="progress" style="height: 20px;">
+                            <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                            80%
+                            </div>
+                        </div>
+                    </div> 
+                    <div class="technologies mb-4">
+                        <p class="">JavaScript</p>
+                        <div class="progress" style="height: 20px;">
+                            <div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                            60%
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+                <div class="col-md-6 px-4">
+                    <div class="technologies mb-4">
+                        <p class="">PHP</p>
+                        <div class="progress" style="height: 20px;">
+                            <div class="progress-bar" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
+                            75%
+                            </div>
+                        </div>
+                    </div>                    
+                    <div class="technologies mb-4">
+                        <p class="">Bootstrap</p>
+                        <div class="progress" style="height: 20px;">
+                            <div class="progress-bar" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                            80%
+                            </div>
+                        </div>
+                    </div> 
+                    <div class="technologies mb-4">
+                        <p class="">WordPress</p>
+                        <div class="progress" style="height: 20px;">
+                            <div class="progress-bar" role="progressbar" style="width: 85%;" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+                            85%
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+            </div>            
+        </div>
+        
+        <!-- Resume -->
+        <div class="page-title px-4 py-3 mt-5">
+            <h3 class="p-2">RESUME <hr></h3>        
+        </div>
+        <div class="resume px-4 py-2">
+            <div class="mt-3 row px-2 mx-0">
+                <!-- Education -->
+                <div class="education col-md-6">
+                    <h4 class="pb-3">Education</h4>
+                    <div class="bachelors left-pane">
+                        <h5 class="">Bachelors of Engineering (B.E) in Information Technology
+                    </div>
+                </div>
+
+                <!-- Work Experience -->
+                <div class="experience col-md-6">
+                    <h4 class="pb-3">Work Experience</h4>
+                    <div class="lti left-pane">
+                        <h5 class="">Software Engineering (B.E) in LTI
+                    </div>
                 </div>
             </div>
         </div>
